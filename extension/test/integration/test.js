55afdb2e9fa944d2b6bd24a42814fb792acec0de
Oliver Müller <oliver+github@ebuuk.de>
2018-05-28 10:04:52 +0200

    CCP-582: initialized repository (#1)
    * CCP-582: initialized repository

---
+++ extension/test/integration/test.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+const {describe, it} = require('mocha')
+
+describe('Extension', () => {
+  it('should have an integration test', () => {
+    assert.equal(true, true)
+  })
+})
