8354b936f79bb3304c6397e97718b89b92038c70
Sofija <sofib@users.noreply.github.com>
2018-05-30 17:23:01 +0200

    CCP-598 Factory methods for a fact finder client. (#6)
    * CCP-598 Factory methods for a fact finder client.
    * CCP-598 Included encoding parameter to factory methods.
    * CCP-598 made assertions in unit tests a bit safer

--- extension/lib/factfinder/Client.js
+++ extension/lib/factfinder/Client.js
@@ -1,6 +1,7 @@
 const SearchAdapter = require('./client/Search')
 const SearchBuilder = require('./client/search/Builder')
 const { DEFAULT_ENCODING } = require('./client/Encoding')
+const { FactFinderAuthentication, AUTHENTICATION_TYPE_EXTENDED } = require('./Authentication')
 
 class FactFinderClient {
   /**
@@ -31,6 +32,52 @@ class FactFinderClient {
    */
   static searchRequestBuilder () {
     return new SearchBuilder()
+  }
+
+  /**
+   * @param {string} baseUri
+   * @param {string|null} [encoding]
+   * @returns {FactFinderClient}
+   */
+  static createPublicClient (baseUri, encoding) {
+    return new FactFinderClient(baseUri, new FactFinderAuthentication(null, null), encoding || DEFAULT_ENCODING)
+  }
+
+  /**
+   * @param {string} baseUri
+   * @param {string} username
+   * @param {string} password
+   * @param {string|null} [encoding]
+   * @returns {FactFinderClient}
+   */
+  static createClientWithSimpleAuthentication (baseUri, username, password, encoding) {
+    return new FactFinderClient(
+      baseUri,
+      new FactFinderAuthentication(username, password),
+      encoding || DEFAULT_ENCODING
+    )
+  }
+
+  /**
+   * @param {string} baseUri
+   * @param {string} username
+   * @param {string} password
+   * @param {string} authenticationPrefix
+   * @param {string} authenticationPostfix
+   * @param {string|null} [encoding]
+   * @returns {FactFinderClient}
+   */
+  static createClientWithExtendedAuthentication ({baseUri, username, password, authenticationPrefix, authenticationPostfix, encoding}) {
+    return new FactFinderClient(
+      baseUri,
+      new FactFinderAuthentication(
+        username,
+        password,
+        AUTHENTICATION_TYPE_EXTENDED,
+        authenticationPrefix,
+        authenticationPostfix),
+      encoding || DEFAULT_ENCODING
+    )
   }
 }
 
